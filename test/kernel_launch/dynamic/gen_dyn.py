13a9dd61bd5ad76177206eca38d1b49de804e1fd
Filip Skogh <filip.skogh@h-partners.com>
2026-02-12 14:58:48 +0000

    feat: fix python mlir code

--- test/kernel_launch/dynamic/gen_dyn.py
+++ test/kernel_launch/dynamic/gen_dyn.py
@@ -2,8 +2,7 @@ from mlir.ir import Context, Location, Module, InsertionPoint
 from mlir.dialects import func, arith, pto, scf
 from mlir.ir import F32Type, IndexType, IntegerType
 
-# python gen_relu_1d.py > relu_1d.pto
-# ptoas --enable-insert-sync relu_1d.pto > gen_cpp1d.cpp
+# python gen_dyn.py | ptoas --enable-insert-sync > generated_relu.cpp
 
 def build():
     with Context() as ctx:
@@ -13,7 +12,7 @@ def build():
             m = Module.create()
 
             tile_w = 32
-            NUM_BLOCKS = 10
+            NUM_BLOCKS = 20
 
             f32 = F32Type.get(ctx)
             u32 = IntegerType.get_signless(32, ctx)
@@ -38,61 +37,61 @@ def build():
                 entry = fn.add_entry_block()
 
             with InsertionPoint(entry):
-                arg0, arg1, argN = entry.arguments
-                c0 = arith.ConstantOp(idx, 0).result
-                c1 = arith.ConstantOp(idx, 1).result
-                c_tile_w = arith.ConstantOp(idx, tile_w).result
-                c_num_blocks = arith.ConstantOp(idx, NUM_BLOCKS).result
+                vec_section = pto.SectionVectorOp()
+                vec_block = vec_section.body.blocks.append()
+                
+                with InsertionPoint(vec_block):
+                    arg0, arg1, argN = entry.arguments
+                    c0 = arith.ConstantOp(idx, 0).result
+                    c1 = arith.ConstantOp(idx, 1).result
+                    c_tile_w = arith.ConstantOp(idx, tile_w).result
+                    c_num_blocks = arith.ConstantOp(u32, NUM_BLOCKS).result
+                    total_elements = arith.IndexCastOp(idx, argN).result
 
-                # cast N to index
-                N_idx = arith.IndexCastOp(idx, argN).result
-                # num_el_per_core = N / NUM_BLOCKS
-                num_el_per_core = arith.DivUIOp(N_idx, c_num_blocks).result
-                # num_tiles = num_el_per_core / tile_w
-                num_tiles = arith.DivUIOp(num_el_per_core, c_tile_w).result
-                bid = arith.IndexCastOp(idx, pto.GetBlockIdxOp()).result
-
-                # GM tensors shape N with stride 1
-                tv0 = pto.MakeTensorViewOp(tv1_f32, arg0, [N_idx], [c1]).result
-                tv1 = pto.MakeTensorViewOp(tv1_f32, arg1, [N_idx], [c1]).result
-
-                # allocate tiles in UB
-                tb0 = pto.AllocTileOp(tile_buf).result
-                tb1 = pto.AllocTileOp(tile_buf).result
-
-                # for loop: for i in range(num_tiles)
-                loop = scf.ForOp(c0, num_tiles, c1)
-                with InsertionPoint(loop.body):
-                    i = loop.induction_variable
-                    """
-                    1000 elements. 100 el per core:
-
-                    core 2 has [200, 300]
-
-                    then for tile offsets we want tile_w*i
-                    """
-                    offset_core = arith.MulIOp(bid, num_el_per_core).result
-                    offset_tile = arith.MulIOp(i, c_tile_w).result
-                    offset_total = arith.AddIOp(offset_core, offset_tile).result
-
-                    sv0 = pto.PartitionViewOp(
-                        tile_view,
-                        tv0,
-                        offsets=[offset_total],
-                        sizes=[c_tile_w]
+                    # must do divisions in u32 for now. until index is supported
+                    num_el_per_core = arith.DivUIOp(argN, c_num_blocks).result
+                    num_tiles = arith.DivUIOp(
+                        num_el_per_core, arith.IndexCastOp(u32, c_tile_w).result
                     ).result
-                    sv1 = pto.PartitionViewOp(
-                        tile_view,
-                        tv1,
-                        offsets=[offset_total],
-                        sizes=[c_tile_w]
-                    ).result
+                    num_el_per_core = arith.IndexCastOp(idx, num_el_per_core).result
+                    num_tiles = arith.IndexCastOp(idx, num_tiles).result
+                    bid = arith.IndexCastOp(idx, pto.GetBlockIdxOp()).result
 
-                    pto.TLoadOp(None, sv0, tb0)
-                    pto.TReluOp(tb0, tb1)
-                    pto.TStoreOp(None, tb1, sv1)
+                    # GM tensors shape N with stride 1
+                    tv0 = pto.MakeTensorViewOp(tv1_f32, arg0, [total_elements], [c1]).result
+                    tv1 = pto.MakeTensorViewOp(tv1_f32, arg1, [total_elements], [c1]).result
 
-                    scf.YieldOp([])
+                    # allocate tiles in UB
+                    tb0 = pto.AllocTileOp(tile_buf).result
+                    tb1 = pto.AllocTileOp(tile_buf).result
+
+                    # for loop: for i in range(num_tiles)
+                    loop = scf.ForOp(c0, num_tiles, c1)
+                    with InsertionPoint(loop.body):
+                        i = loop.induction_variable
+                        offset_core = arith.MulIOp(bid, num_el_per_core).result
+                        offset_tile = arith.MulIOp(i, c_tile_w).result
+                        offset_total = arith.AddIOp(offset_core, offset_tile).result
+
+                        # each core c takes a tile at offset c*nun_el_per_core+i*tile_w  
+                        sv0 = pto.PartitionViewOp(
+                            tile_view,
+                            tv0,
+                            offsets=[offset_total],
+                            sizes=[c_tile_w]
+                        ).result
+                        sv1 = pto.PartitionViewOp(
+                            tile_view,
+                            tv1,
+                            offsets=[offset_total],
+                            sizes=[c_tile_w]
+                        ).result
+
+                        pto.TLoadOp(None, sv0, tb0)
+                        pto.TReluOp(tb0, tb1)
+                        pto.TStoreOp(None, tb1, sv1)
+
+                        scf.YieldOp([])
 
                 func.ReturnOp([])
 
